c28c96d7a683858c3ec2a8c2cfda0fcfad12a65a
MauroGBecares <maurobecares26@gmail.com>
2024-06-30 22:01:33 -0300

    Hooks2

--- src/components/authors/Autoras.jsx
+++ src/components/authors/Autoras.jsx
@@ -1,10 +1,37 @@
-import React from 'react'
-import { authors } from '../../data/authors'
+import React, { useState, useEffect } from 'react'
 import { Container, Row } from 'react-bootstrap'
 import './Autoras.css'
 import Autora from '../autora/Autora'
+import useModal from '../../hooks/useModal'
+import ModalPage from '../modalPage/ModalPage'
 
 const Autoras = () => {
+  //https://localhost:7069/api/Authors
+  const [authors, setAuthors] = useState([])
+  const [titleModal, setTitleModal] = useState('')
+  const [bodyModal, setBodyModal] = useState('')
+  const { isShown, showModal, hideModal } = useModal()
+  useEffect(() => {
+    fetch("https://localhost:7069/api/Authors", {
+      method: "GET",
+      mode: "cors",
+    })
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error("Error al obtener los autoras");
+        }
+        return response.json();
+      })
+      .then((authorsData) => {
+        setAuthors(authorsData);
+      })
+      .catch((error) => {
+        console.error("Error:", error);
+        setTitleModal('Error')
+        setBodyModal(error.message)
+        showModal()
+      });
+  }, []);
   return (
     <>
       <Container fluid="sm">
@@ -16,11 +43,17 @@ const Autoras = () => {
             <Autora
               key={author.id}
               name={author.name}
-              img={author.img}
+              img={author.imageUrl}
             />
           ))}
         </Row>
       </Container>
+      <ModalPage
+        title={titleModal}
+        body={bodyModal}
+        show={isShown}
+        onClose={hideModal}
+      />
     </>
   );
 };
--- src/components/autora/Autora.jsx
+++ src/components/autora/Autora.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
 import { Col, Card } from 'react-bootstrap'
 import PropTypes from 'prop-types';
+import './Autora.css'
 
 const Autora = ({ name, img }) => {
   return (
     <>
       <Col xs={6} md={4} lg={3} className='d-flex flex-column justify-content-between mb-4'>
-        <Card className="bg-dark text-white">
+        <Card className="bg-dark text-white container-img">
           <Card.Img src={img} alt={name} />
           <Card.ImgOverlay>
             <Card.Title>{name}</Card.Title>
